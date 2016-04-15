f3ede33c00bebde92272ad55bffc45acbc574404
Jason Yost <jyost@gocortexlabs.com>
2016-04-14 20:00:56 -0600

    Get fingerprint method

--- lib/file_compare.rb
+++ lib/file_compare.rb
@@ -1,5 +1,17 @@
-require "file_compare/version"
+require 'file_compare/version'
+require 'digest/sha2'
 
 module FileCompare
-  # Your code goes here...
+  # Public: Returns SHA-1 finger print of provided file
+  #
+  # file_path  - Path to the file
+  #
+  # Examples
+  #  FileCompare.get_fingerprint(file_path)
+  #  => 069f850d97ac8e2d731aaad52b0d090e47c483ee
+  #
+  # Returns string
+  def self.get_fingerprint(file_path)
+    Digest::SHA256.file(file_path).hexdigest
+  end
 end
--- spec/file_compare_spec.rb
+++ spec/file_compare_spec.rb
@@ -3,11 +3,26 @@ SimpleCov.start
 require 'spec_helper'
 
 describe FileCompare do
+
+  let(:test_text_file) { 'spec/support/test_text.txt' }
+  let(:left_matching_file) { 'spec/support/left_file.txt' }
+  let(:right_matching_file) { 'spec/support/right_file.txt' }
+
   it 'has a version number' do
     expect(FileCompare::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'get_fingerprint' do
+    it 'should not raise a NoMethodError' do
+      expect { FileCompare.get_fingerprint(test_text_file) }.not_to raise_error
+    end
+
+    context 'given a valid file path' do
+      it 'should return an SHA256 hash as a string' do
+        f = FileCompare.get_fingerprint(test_text_file)
+        expect(f).to be_a String
+        expect(f).to eq(Digest::SHA256.file(test_text_file).hexdigest)
+      end
+    end
   end
 end
